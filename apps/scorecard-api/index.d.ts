4b509d27dcda1c9361c22ce7842e7d1b583d9e72
Jason Humphrey <Humphrey.jason32@gmail.com>
2025-06-19 10:21:24 -0500

    feat(api): add optional autoParse

--- apps/scorecard-api/index.d.ts
+++ apps/scorecard-api/index.d.ts
@@ -1,4 +1,12 @@
+export interface FetchApiOptions {
+  params?: Record<string, any>;
+  token?: string;
+  headers?: Record<string, string>;
+  fields?: string[];
+  /** Enable or disable automatic parsing of the response. Default: true */
+  autoParse?: boolean;
+}
 export function fetchApiData(
   url: string,
-  params?: Record<string, any>
-): Promise<Record<string, number>>;
+  options?: FetchApiOptions
+): Promise<any>;
